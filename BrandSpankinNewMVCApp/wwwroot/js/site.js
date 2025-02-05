1ffefc3bc6fa5acc749dee2aec66358f7597bba9
AutPort <ajs289@byu.edu>
2025-02-05 15:33:59 -0700

    added jQuery by putting my javaScript inside my site.js file instead of writing it out on my cshtml pages

--- BrandSpankinNewMVCApp/wwwroot/js/site.js
+++ BrandSpankinNewMVCApp/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Write your JavaScript code.
+$(document).ready(function () {
 
-// Write your JavaScript code.
+    $("#calculatorForm button").click(function () {
+
+        var hours = $("#hoursNeeded").val();
+        var hourlyRate = parseFloat($("#hourlyRate").text()); // Get hourly rate from hidden span
+
+        // Validate input
+        if (hours <= 0 || isNaN(hours)) {
+            $("#error-message").show();
+            return;
+        } else {
+            $("#error-message").hide();
+        }
+
+        // Calculate total
+        var total = hours * hourlyRate;
+
+        // Display result
+        $("#totalCost").val(total.toFixed(2));
+    });
+});
